d93cbca135844187252ff8a63891e5f70c86bca3
User <g44l44ng@gmail.com>
2023-09-30 11:25:17 +0700

    menambahkan contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+Class contoh1            extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan<h1>";
+        echo "Nama Saya Muhammad Galang Saputra dengan NIM 19220781
+              Saya tinggal di daerah Bekasi 
+              Olahraga yang paling kusukai adalah 
+              Balet"; 
+    }
+}
